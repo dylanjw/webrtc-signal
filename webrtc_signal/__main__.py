f017d7b10c5f03e54abd02f67fa528ddb33f0a67
Dylan Wilson <dylanjw@protonmail.com>
2020-06-04 19:35:59 -0700

    implement basic ws server

---
+++ webrtc_signal/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from aiohttp import web
+
+async def websocket_handle(request):
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+
+    return ws
+
+app = web.Application()
+app.add_routes([web.get('/ws', websocket_handle)])
+
+web.run_app(app)
